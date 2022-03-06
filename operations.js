a98a0b457163ffd25b13067311679d1808262470
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-06 17:14:35 +0000

    add math operations

---
+++ operations.js
@@ -0,0 +1,13 @@
+ //const sum = 10 + 0;
+ //JavaScript uses the + symbol as an addition operator when placed between two numbers.
+
+ var a = 10;
+ var b = 2;
+
+ const sum = a+b;
+ const difference = a-b;
+ const product = a*b;
+ const quotient = a/b
+
+ console.log(sum, difference, product, quotient);
+ 
