48f6e9444864c7ab2f060a6d48927f480b35b0af
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 18:46:13 +0000

    add length

---
+++ string.js
@@ -0,0 +1,8 @@
+//You can find the length of a String value by writing .length after the string variable or string literal.
+
+let lastNameLength = 0;
+const lastName = "Bencardino";
+
+lastNameLength = lastName;
+console.log("My last name is " + lastName);
+console.log(lastName.length + " characters");
