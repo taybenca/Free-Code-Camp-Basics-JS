5ccde31aab3b0adb209220adb317893e2a6a9240
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 18:29:54 +0000

    add string and variables

--- quotes.js
+++ quotes.js
@@ -9,3 +9,8 @@ console.log(myStr);
 //adding three lines without use spaces, second line must have a backslash and tab.
 const newStr = "FristLine\n\t\\SecondLine\nThirdLine"
 console.log(newStr);
+
+//constructing strings with variables
+const myName = "Tayanne";
+const phrase = "Hello, my name is " + myName + ", and I am well!"
+console.log(phrase);
