d85671175795b719204cdf68fe0ef86080554b3a
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 17:36:53 +0000

    removing escape characters

--- quotes.js
+++ quotes.js
@@ -1,5 +1,7 @@
 //Escaping literal quotes in strings
-
 const myString = "I am a \"double quoted\" string inside \"double quotes\".";
-
 console.log(myString);
+
+//removing the escape characters
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);
