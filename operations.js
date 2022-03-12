d8d7796236c247c404a6133c1944dd62ebf925f8
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 17:07:20 +0000

    add comments

--- operations.js
+++ operations.js
@@ -3,16 +3,16 @@
  var a = 10;
  var b = 2;
 
- const sum = a+b;
- const difference = a-b;
- const product = a*b;
- const quotient = a/b
+ const sum = a+b;  //we can also use +=
+ const difference = a-b; //we can also use -=
+ const product = a*b; //we can also use *=
+ const quotient = a/b  //we can also /=
 
  console.log(sum, difference, product, quotient);
  
  //decimal numbers
  var a = 0.6
- var b = 0.2
+ var b = 0.2 
 
  const sumDec = a+b;
  const differenceDec = a-b;
