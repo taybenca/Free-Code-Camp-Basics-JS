f39e074180095d653bf81370ca4d41626b043867
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 15:37:05 +0000

    check if the number is even or odd

---
+++ remainder.js
@@ -0,0 +1,14 @@
+// The remainder operator is sometimes incorrectly referred to as the modulus operator. It is very similar to modulus, but does not work properly with negative numbers.
+
+var remainder = 11%2;
+
+console.log("The remainder is: " + remainder);
+
+// In mathematics, a number can be checked to be even or odd by checking the remainder of the division of the number by 2
+
+if (remainder = 1){
+    console.log("This number is odd")
+}
+else {
+    console.log("This numer is even")
+}
