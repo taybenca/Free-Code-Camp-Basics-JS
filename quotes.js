a42f5c4a164df777c2a1ff5ae240e353d58857bb
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-12 18:01:35 +0000

    add three lines using backslash

--- quotes.js
+++ quotes.js
@@ -5,3 +5,7 @@ console.log(myString);
 //removing the escape characters
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 console.log(myStr);
+
+//adding three lines without use spaces, second line must have a backslash and tab.
+const newStr = "FristLine\n\t\\SecondLine\nThirdLine"
+console.log(newStr);
