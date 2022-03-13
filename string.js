53996b740f45e9f03c550c15d14a0623e83ec044
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-13 10:24:22 +0000

    add bracket notation

--- string.js
+++ string.js
@@ -9,3 +9,12 @@ console.log(lastName.length + " characters");
 
 let firstLetter = lastName[0];
 console.log("The first letter is " + "'" + firstLetter + "'");
+
+let thirdLetter = lastName[2];
+console.log(thirdLetter);
+
+let lastLetter = lastName[lastName.length - 1]
+console.log(lastLetter);
+
+let thridLastLetter = lastName[lastName.length -3]
+console.log(thridLastLetter);
