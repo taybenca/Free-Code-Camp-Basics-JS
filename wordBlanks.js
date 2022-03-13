b9e606d88da35f33d0dc5a32dfdbdd822b7f6e09
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-13 10:39:44 +0000

    add word blanks

---
+++ wordBlanks.js
@@ -0,0 +1,7 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = "A " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb+".";
+console.log(wordBlanks);
