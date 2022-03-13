860f5937dbd0a3716486dd1aaf4b17990cf88fa7
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-03-13 11:36:15 +0000

    add items

---
+++ shoppingList.js
@@ -0,0 +1,12 @@
+//create a shooping list with 5 itens.
+//frist elements in each sub-arrays should be a string
+//the second element in each sub-arrays should be numbers
+
+const list = [
+    ["grapes", 2],
+    ["cheese", 4],
+    ["Eggs", 12],
+    ["chicken", 1],
+    ["bacon", 3]
+]
+console.log(list);
